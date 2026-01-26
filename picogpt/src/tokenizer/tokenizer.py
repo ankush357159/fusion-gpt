fcb471dfc770728a4812bbcb7403c716f9c4aafe
Wankar, Ankush <Ankush.Wankar@ellucian.com>
2026-01-26 15:09:23 +0530

    Tokenizer

--- picogpt/src/tokenizer/tokenizer.py
+++ picogpt/src/tokenizer/tokenizer.py
@@ -4,22 +4,26 @@ import tiktoken
 class TiktokenTokenizer:
     def __init__(
         self,
-        encoding_name="cl100k_base",
-        bos_token_id=None,
-        eos_token_id=None,
+        encoding_name="gpt2",
+        bos_token_id=50256,
+        eos_token_id=50256,
+        pad_token_id=50256,
     ):
         self.enc = tiktoken.get_encoding(encoding_name)
         self.vocab_size = self.enc.n_vocab
+
+        # GPT-2 uses <|endoftext|> for BOS/EOS/PAD conventionally
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
+        self.pad_token_id = pad_token_id
 
     def encode(
         self,
         text,
         add_bos=False,
         add_eos=False,
-        allowed_special=(),
-        disallowed_special="all",
+        allowed_special={"<|endoftext|>"},
+        disallowed_special=(),
     ):
         tokens = self.enc.encode(
             text,
@@ -28,13 +32,8 @@ class TiktokenTokenizer:
         )
 
         if add_bos:
-            if self.bos_token_id is None:
-                raise ValueError("bos_token_id is not set")
             tokens = [self.bos_token_id] + tokens
-
         if add_eos:
-            if self.eos_token_id is None:
-                raise ValueError("eos_token_id is not set")
             tokens = tokens + [self.eos_token_id]
 
         return tokens
@@ -42,36 +41,18 @@ class TiktokenTokenizer:
     def decode(self, tokens):
         return self.enc.decode(tokens)
 
-    def encode_batch(
-        self,
-        texts,
-        add_bos=False,
-        add_eos=False,
-        allowed_special=(),
-        disallowed_special="all",
-    ):
-        return [
-            self.encode(
-                text,
-                add_bos=add_bos,
-                add_eos=add_eos,
-                allowed_special=allowed_special,
-                disallowed_special=disallowed_special,
-            )
-            for text in texts
-        ]
+    def encode_batch(self, texts, **kwargs):
+        return [self.encode(text, **kwargs) for text in texts]
 
     def decode_batch(self, batch_tokens):
         return [self.decode(tokens) for tokens in batch_tokens]
 
     def truncate(self, tokens, max_length):
-        if max_length is None:
-            return tokens
-        return tokens[:max_length]
+        return tokens if max_length is None else tokens[:max_length]
 
-    def pad_to_max_length(self, tokens, max_length, pad_token_id=0):
+    def pad_to_max_length(self, tokens, max_length):
         if max_length is None:
             return tokens
         if len(tokens) >= max_length:
             return tokens[:max_length]
-        return tokens + [pad_token_id] * (max_length - len(tokens))
+        return tokens + [self.pad_token_id] * (max_length - len(tokens))
